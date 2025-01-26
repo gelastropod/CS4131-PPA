f4d04b824bd12b748d29d3bf905eb004a26cb4f8
DevNerdGR <tewgrc172@gmail.com>
2025-01-26 18:44:24 +0800

    THEME WORKS NOW LETSGOOOOOOOO YAYYYYY

--- app/src/main/java/com/example/cs4131_ppa/MainActivity.kt
+++ app/src/main/java/com/example/cs4131_ppa/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -21,7 +25,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CS4131PPATheme {
-                MainApp()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainApp()
+                }
             }
         }
     }
